0f8449e500484c94a1d1bfaeece8df6066d3c705
vijayalakshmiTanniru <vijayalakshmitanniru98@gmail.com>
2025-06-29 16:24:06 +0530

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+const Footer = () => (
+  <Box textAlign="center" mt={5} mb={3}>
+    <Typography variant="body2" color="textSecondary">
+      © 2025 Fitness Tracker Dashboard
+    </Typography>
+  </Box>
+);
+
+export default Footer;
